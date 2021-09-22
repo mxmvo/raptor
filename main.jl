945b527e2f6e66a0800b8256d77ee96fde7d1d4e
Maxim Oldenbeek <maximoldenbeek@Maxims-MacBook-Pro.local>
2021-09-22 14:36:28 +0200

    first comit

---
+++ main.jl
@@ -0,0 +1,92 @@
+#%%
+
+using DataFrames
+using CSV
+using PlotlyJS
+using Revise
+using BenchmarkTools
+
+#%%
+
+includet("raptor.jl")
+
+#%%
+
+routes = CSV.read("gtfs-openov-nl/routes.txt", DataFrame)
+trips = CSV.read("gtfs-openov-nl/trips.txt", DataFrame)
+stops = CSV.read("gtfs-openov-nl/stops.txt", DataFrame)
+stop_times = CSV.read("gtfs-openov-nl/stop_times.txt", DataFrame)
+shapes = CSV.read("gtfs-openov-nl/shapes.txt", DataFrame);
+ 
+#%%
+
+function parse_time(t)
+    h,m,s = parse.(Int, split(t,":"))
+    return h*3600+m*60+s
+end
+
+function get_parent_station(s_id)
+    a = stops[stops.stop_id .== s_id,:parent_station][1]
+    ismissing(a) ? s_id : a
+end
+
+#%%
+
+transform!(stop_times, :arrival_time => (t -> parse_time.(t)) => :arrival_s)
+transform!(stop_times, :stop_id => (t -> string.(t) ) => :stop_id)
+
+stops.parent_id = get_parent_station.(stops[:,:stop_id])
+# transform!(stop_times, :stop_id => (t -> get_parent_station.(t)) => :parent_station)
+
+#%%
+
+function plot_stops(lat, lon)
+    marker = attr(size=1, color="black")
+    trace = scattergeo(;mode ="markers",
+                        lat = lat,
+                        lon = lon,
+                        marker = marker)
+
+    geo = attr(scope = "europe",
+                lataxis_range = [50.,54.],
+                lonaxis_range = [2,7])
+    layout = Layout(;title = "Stops Netherlands", showlegend = false, geo = geo)
+    plot(trace, layout)
+end
+
+# plot_stops(stops[:,:stop_lat], stops[:,:stop_lon])
+
+# %%
+
+g_stops = groupby(stop_times, :stop_id)
+g_trips = groupby(stop_times, :trip_id)
+
+dep_id = "1331361"
+dep_time = parse_time("12:45:00")
+arr_id = "2324634"
+
+# %%
+
+df_stop = DataFrame()
+df_stop[!,:stop_id] = stops[:,:stop_id]
+df_stop[!,:arrival_s] .= Inf32
+df_stop[!,:parent] .= ""
+df_stop[!,:marked] .= false
+df_stop[!,:trip] .= zero(Int)
+
+df_stop[df_stop.stop_id .== dep_id,[:arrival_s, :marked]] = [dep_time true]
+
+# %%
+
+for T ∈ 1:10
+    new_nodes = stage_2(df_stop, g_stops, g_trips, stops)
+    update_df!(df_stop, new_nodes)
+    println(df_stop[df_stop.stop_id .== arr_id,:])
+end
+
+# %%
+
+
+get_trip(df_stop,stops, arr_id)
+
+#%%
---
+++ raptor.jl
@@ -0,0 +1,104 @@
+#%%
+
+using DataFrames
+using CSV
+using PlotlyJS
+
+#%%
+function select_minrow(t, p, tr)
+    i = argmin(t)
+    return [t[i] p[i] tr[i]]
+end
+
+function make_Q(df, df_sr)
+    Q = Dict()
+    for row ∈ eachrow(df[df.marked .== true,:])
+        p = row.stop_id
+        
+        for rrow ∈ eachrow(df_sr[df_sr.stop_id .== p,:])
+            r = rrow.route_id
+            s = rrow.stop_sequence
+            if r ∈ keys(Q)
+                _, ss = Q[r]
+                if s < ss
+                    Q[r] = (p,s)
+                end
+            else
+                Q[r] = (p,s)
+            end
+        end
+    end
+    Q
+
+end
+
+function stage_2(df, g_stops, g_trips, stops; τ⁺ = Inf32)
+    
+    # Run over the marked stops
+    potential_new_stops = []
+
+    for r ∈ eachrow(df[df.marked .== true,[:stop_id,:arrival_s]])
+        s_id, τ = r.stop_id, r.arrival_s
+        
+        # Find the trips through this stop after the arrival time 
+        ((stop_id = s_id,) ∉ keys(g_stops)) && continue
+
+        dff = g_stops[(s_id,)]
+        trps = unique(filter(:arrival_s => (t -> t > τ), dff)[:,:trip_id])
+        
+        # Different platforms?
+        p_id = stops[stops.stop_id .== s_id, :parent_station][1]
+        if !ismissing(p_id)
+            dfp = stops[isequal.(stops.parent_station, p_id),[:stop_id]]
+            dfp[!,:arrival_s] .= τ + 120 #2 min overstap tijd
+            dfp[!,:parent] .= s_id
+            dfp[!,:trip] .= -1
+            append!(potential_new_stops, [dfp])
+        end
+
+        # For those trips find the stops that can be reached. 
+        for t_id ∈ trps
+            dff = g_trips[(t_id,)]
+            τ_stop = dff[dff.stop_id .== s_id, :arrival_s][1]
+            stp = filter(:arrival_s => (t -> t > τ_stop), dff)[:,[:stop_id,:arrival_s]]
+            stp[!,:parent] .= s_id
+            stp[!,:trip] .= t_id
+            append!(potential_new_stops, [stp])
+        end
+    end
+
+    # Add all the stops to a DataFrame
+    # Select the minimal arrival time per stop. 
+    dff = vcat(potential_new_stops...)
+    println(size(dff))
+    combine(groupby(dff,:stop_id), [:arrival_s, :parent,:trip] => select_minrow => [:arrival_s, :parent,:trip])
+end
+
+
+function update_df!(df, new_stops)
+    df[!,:marked] .= false
+    for r ∈ eachrow(new_stops)
+        if r.arrival_s < df[df.stop_id .== r.stop_id,:arrival_s][1]
+            df[df.stop_id .== r.stop_id, [:arrival_s,:marked,:parent,:trip]] = [r.arrival_s true r.parent r.trip]
+        end    
+    end
+end
+
+function get_trip(df, stops, arr_id)
+
+    t = [arr_id]
+    p = df[df.stop_id .== arr_id,:parent][1]
+    while p !== ""
+        append!(t,[p])
+        p = df[df.stop_id .== p,:parent][1]
+    end
+    dff = sort(filter(r -> r.stop_id ∈ t, df), :arrival_s)
+    names = []
+    for r ∈ eachrow(dff)
+        n = stops[stops.stop_id .== r.stop_id,:stop_name][1]
+        append!(names, [n])
+    end
+    dff.name = names
+    return dff
+    # transform(dff, :stop_id => (t -> stops[stops.stop_id .== t, :stop_name]) => :stop_name)
+end
